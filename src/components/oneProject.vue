34f48a0f1105809431c81a064e1475592078d4dc
EDG\astrid.bossy <bossy.astrid@gmail.com>
2018-07-23 15:07:47 +0200

    modif du style

--- src/components/oneProject.vue
+++ src/components/oneProject.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
+        <div class="col col-md-4 col-lg-4">
         <div class="bord">
-            <h1>{{toto.name}}</h1>
+            <h1>{{toto.name}}</h1><br>
                 <img :src="toto.picture"><br>
-                    <router-link id="button" class="btn btn-success" to="/details">Consulter le Projet</router-link>
+                    <router-link  id="button" class="btn btn-info" :to="{name:'detailsProject', params:{totoDetails:toto,id: toto.id}}">Consulter le Projet</router-link><br>
                     <li :class="{ active: toto.isActive }"><toggle-button :value="toto.isActive" @change="toto.isActive = !toto.isActive" :key="toto.id"/></li>
                     <h2 v-if="toto.isActive === true" class="green"> Actif </h2>
                     <h2 v-if="toto.isActive  === false" class="red"> Inactif </h2><br>
+                    </div>
         </div>
     </div>
 </template>
@@ -14,19 +16,14 @@
 <script>
 import detailsProject from "./detailsProject.vue";
 export default {
-  name: 'oneProject', 
-  props:['toto'], // mot clé qui va me permettre d'acceder aux infos listeProjets.
-  data () {
-    return {
- 
-  } 
-},
+  name: "oneProject",
+  props: ["toto"], // mot clé qui va me permettre d'acceder aux infos listeProjets.
+  data() {
+    return {};
+  },
 
-methods:{}
-
-
+  methods: {}
 };
-
 </script>
 
 
@@ -36,27 +33,25 @@ methods:{}
 .bord {
   margin: 0 auto;
   margin-top: 5px;
-  border-radius: 25px;
+  margin-bottom: 20px;
+  border-radius: 5px;
   border: 2px solid #0f0f0f;
-  padding: 20px; 
-  width: 500px;
-  height: 200px; 
-
+  padding: 20px;
+  width: 300px;
+  height: 600px;
 }
 
 .green {
-   color: rgb(8, 192, 8) 
+  color: rgb(8, 192, 8);
 }
 .red {
-    color: firebrick
+  color: firebrick;
 }
 
 #button {
-  background-color: lightblue;
-  font-size: 20px;
-  color:blue;
+  font-size: 15px;
+  color: white;
   margin-top: 5%;
-  
+  margin-bottom: 5%;
 }
-
 </style>
