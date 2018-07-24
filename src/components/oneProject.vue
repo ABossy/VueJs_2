89767d35452cbf1d4bc3d36519666882ba1f3815
EDG\astrid.bossy <bossy.astrid@gmail.com>
2018-07-24 17:04:18 +0200

    modification suite a l'ajout de l'api

--- src/components/oneProject.vue
+++ src/components/oneProject.vue
@@ -1,28 +1,52 @@
 <template>
     <div>
         <div class="col col-md-4 col-lg-4">
-        <div class="bord">
-            <h1>{{toto.name}}</h1><br>
-                <img :src="toto.picture"><br>
+            
+          <div class="bord">
+            <h3>{{toto.name}}</h3><br>
+                <img :src="toto.creator.picture"><br>
                     <router-link  id="button" class="btn btn-info" :to="{name:'detailsProject', params:{totoDetails:toto,id: toto.id}}">Consulter le Projet</router-link><br>
-                    <li :class="{ active: toto.isActive }"><toggle-button :value="toto.isActive" @change="toto.isActive = !toto.isActive" :key="toto.id"/></li>
-                    <h2 v-if="toto.isActive === true" class="green"> Actif </h2>
-                    <h2 v-if="toto.isActive  === false" class="red"> Inactif </h2><br>
-                    </div>
+                    <p>{{toto.description}}</p>
+                    <button id="back"  @click="deleteProjet(toto.id)" class="btn btn-danger">Delete</button>
+          </div>
         </div>
     </div>
+    
 </template>
 
 <script>
+import axios from "axios";
 import detailsProject from "./detailsProject.vue";
 export default {
   name: "oneProject",
   props: ["toto"], // mot clé qui va me permettre d'acceder aux infos listeProjets.
   data() {
-    return {};
+    return {
+      hideOffProjet: false,
+      projets: [], // au début la liste des machines est vide
+      loading: false,
+      error: null
+    };
   },
 
-  methods: {}
+  methods: {
+    deleteProjet: function(id) {
+      axios
+        .delete(
+          "https://daily-standup-campus.herokuapp.com/api/projects/" + id,
+          {
+            headers: {
+              Authorization:
+                "Bearer " + localStorage.getItem('userTokenKey')
+                //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViMjNmODIzYTM5YjlmMDAxNGViNGJlNiIsImlhdCI6MTUzMTE0Mjg1MX0.K5e_nO1kl0sOOK8rvjYTiRkHPk2vBoGcSGY0Xh3zVQg"
+            }
+          }
+        )
+        .then(function(reponse) {
+          alert("delete creation");
+        });
+    }
+  }
 };
 </script>
 
@@ -38,7 +62,7 @@ export default {
   border: 2px solid #0f0f0f;
   padding: 20px;
   width: 300px;
-  height: 600px;
+  height: 850px;
 }
 
 .green {
